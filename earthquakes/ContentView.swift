99aba17770fdfd25f03fb32d40ff28e168080f3f
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2020-07-03 12:36:59 -0700

    Initial Commit

---
+++ earthquakes/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  earthquakes
+//
+//  Created by Tim Roesner on 7/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
