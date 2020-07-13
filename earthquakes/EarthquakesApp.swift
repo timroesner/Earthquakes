d842798e885d412a21af3fa68b2c9371e8f7575a
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2020-07-12 22:56:12 -0700

    Switch to SwiftUI based App

---
+++ earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Tim Roesner on 7/9/20.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ListView()
+        }
+    }
+}
